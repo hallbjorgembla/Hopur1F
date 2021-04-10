c5d1abdd1b5a8b8ec7c2f62b0da6ecc6f795216f
JuliaSifOlafs <77738911+JuliaSifOlafs@users.noreply.github.com>
2021-04-10 16:30:50 +0000

    search service

--- src/FlightReservation/controller/SearchService.java
+++ src/FlightReservation/controller/SearchService.java
@@ -1,8 +1,49 @@
 package FlightReservation.controller;
 
-public class SearchService {
+import FlightReservation.model.Flight;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class SearchService implements SearchServiceInterface {
     BookingDBManager bookingDBManager;
 
     public SearchService() {
+    }
+
+    @Override
+    public ArrayList<Flight> findAll() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Flight> findFlights(LocalDateTime flightDate, String departureCity, String arrivalCity) {
+        return null;
+    }
+
+    @Override
+    public Optional<Flight> findFlightById(int flightId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public ArrayList<Flight> sortByPrice(ArrayList<Flight> flights) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Flight> sortByDepartureTime(ArrayList<Flight> flights) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Flight> sortByArrivalTime(ArrayList<Flight> flights) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Flight> makeFlights() {
+        return null;
     }
 }
